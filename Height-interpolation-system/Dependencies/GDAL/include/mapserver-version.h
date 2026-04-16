d7bd6ea0e040ba50c2b3ffadbceee6ad2ffa79d9
Stepan N <stepan.nogtev.3@gmail.com>
2026-04-17 01:22:16 +0300

    Add_D+GDAL_lib
    Добавил зависимость к этой библиотеке, чтобы импортировать с tiff файлами

---
+++ Height-interpolation-system/Dependencies/GDAL/include/mapserver-version.h
@@ -0,0 +1,11 @@
+#ifndef _MAPSERVER_VERSION_H
+#define _MAPSERVER_VERSION_H
+
+#define MS_VERSION_MAJOR 8
+#define MS_VERSION_MINOR 6
+#define MS_VERSION_REV 0
+#define MS_VERSION "8.6.0"
+#define MS_VERSION_NUM  80600 
+
+#endif
+
--- Height-interpolation-system/Height-interpolation-system/src/main.cpp
+++ Height-interpolation-system/Height-interpolation-system/src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "tiffio.h"
 
 int main()
 {
+    setlocale(LC_ALL, "ru");
+    const char* version = TIFFGetVersion();
 
+    if (version)
+    {
+        std::cout << "LibTIFF успешно подключена!" << std::endl;
+        std::cout << "Версия: " << version << std::endl;
+    }
+    else
+    {
+        std::cerr << "Ошибка: Не удалось получить данные из библиотеки." << std::endl;
+        return 1;
+    }
 }
