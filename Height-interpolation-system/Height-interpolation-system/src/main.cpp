7e273774bc32c1abe2352e3dec3d21bd9c24d2f3
Stepan N <stepan.nogtev.3@gmail.com>
2026-04-17 12:40:24 +0300

    Add import image til

--- Height-interpolation-system/Height-interpolation-system/src/main.cpp
+++ Height-interpolation-system/Height-interpolation-system/src/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include "tiffio.h"
+#include <tiffio.h>
 
 int main()
 {
     setlocale(LC_ALL, "ru");
-    const char* version = TIFFGetVersion();
+    TIFF* tif = TIFFOpen("src/Amerika1.tif", "r"); //Имя tiff файла
+    if (!tif) return 1;
 
-    if (version)
-    {
-        std::cout << "LibTIFF успешно подключена!" << std::endl;
-        std::cout << "Версия: " << version << std::endl;
-    }
-    else
-    {
-        std::cerr << "Ошибка: Не удалось получить данные из библиотеки." << std::endl;
-        return 1;
-    }
+    uint32_t width, height;
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
+
+    // Выделяем память под одну строку данных
+    tdata_t buf = _TIFFmalloc(TIFFScanlineSize(tif));
+
+    // Читаем первую строку (индекс 0)
+    TIFFReadScanline(tif, buf, 0);
+
+    // Преобразуем буфер в массив целых чисел
+    int16_t* data = (int16_t*)buf;
+
+    std::cout << "Разрешение: " << width << "x" << height << std::endl;
+    std::cout << "Высота в точке (0,0): " << data[0] << " метров" << std::endl;
+
+    _TIFFfree(buf);
+    TIFFClose(tif);
 }
